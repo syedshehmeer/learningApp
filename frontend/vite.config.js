50312662799154fec583b9ae348527e5a2ec8aab
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2024-02-25 08:56:26 +0500

    port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5003,
     proxy: {
       "/api": {
         target: "https://learning-app-backend.vercel.app/",
