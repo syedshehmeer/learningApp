8894c0ee87a589f85231f2ae80a81f2815d90050
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2024-02-25 08:34:52 +0500

    backend changes

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
 import cookieParser from "cookie-parser";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import connectDB from "./config/db.js";
+import {
+  notFound,
+  errorHandler,
+} from "./backend/middleware/errorMiddleware.js";
+import connectDB from "./backend/config/db.js";
 const port = process.env.PORT || 5000;
-import userRoutes from "./routes/userRoutes.js";
+import userRoutes from "./backend/routes/userRoutes.js";
 
 connectDB();
 
