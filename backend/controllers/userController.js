c83b69fdda22030a98353096aa15743522279d28
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2024-02-25 15:23:50 +0500

    checking

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,9 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    generateToken(res, user._id);
+    generateToken(res, user._id)
+      ? console.log("token generated")
+      : console.log("Failure jwt");
     res.status(201).json({
       _id: user._id,
       name: user.name,
