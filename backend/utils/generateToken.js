029f32d30658d724fc9224c20f92ac530ab19900
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2024-02-25 15:00:41 +0500

    generate token change

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId) => {
   });
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
