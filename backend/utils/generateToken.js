6c32d2831937de213074bcca11ab9e0905d93fab
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2024-02-25 15:16:09 +0500

    checking

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,13 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-  res.cookie("jwt", token);
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+  console.log("in generate token");
 };
 
 export default generateToken;
