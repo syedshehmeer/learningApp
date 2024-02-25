6c801562f485e5c63d366c963edce2d5a72d248f
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2024-02-25 09:25:36 +0500

    changes

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://learning-app-backend.vercel.app/",
+});
 
 export const apiSlice = createApi({
   baseQuery,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5003,
+    port: 3000,
     proxy: {
       "/api": {
         target: "https://learning-app-backend.vercel.app/",
