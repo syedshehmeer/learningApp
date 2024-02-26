8d169bc6d2e295c5ee8964805049b95aeac7470c
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2024-02-26 21:50:02 +0500

    change

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,25 +1,20 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-  if (process.env.NODE_ENV === "production") {
+  try {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
-  } else {
-    // Set other cookie attributes for development (without secure)
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+  } catch (error) {
+    console.error("Error generating token:", error);
+    // Handle the error appropriately, e.g., return an error response
   }
-  console.log("in generate token");
 };
 
 export default generateToken;
